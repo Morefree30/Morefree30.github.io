e345a2f5fbedea6181d7bc044b3cfaab683b775f
Morefree30 <97297385+Morefree30@users.noreply.github.com>
2023-02-14 00:44:25 +0000

    🎉 INT COMMIT: Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/7a1.html","posts/211e.html","posts/f96a.html","posts/be2e.html","posts/d90e.html","posts/dc0b.html","posts/7d00.html","posts/48b9.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/211e.html","posts/f96a.html","posts/be2e.html","posts/d90e.html","posts/dc0b.html","posts/7d00.html","posts/48b9.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
